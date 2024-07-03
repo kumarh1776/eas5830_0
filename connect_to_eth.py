5d9dd905bf546a282cb66830f2b9aa4bee715e1a
kumarh1776 <59850401+kumarh1776@users.noreply.github.com>
2024-07-02 22:06:39 -0400

    Add files via upload

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -26,15 +26,15 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-    bnb_url = "https://data-seed-prebsc-1-s1.binance.org:8545"
-    w3 = Web3(HTTPProvider(bnb_url))
-    assert w3.is_connected(), f"Failed to connect to provider at {bnb_url}"
+  bnb_url = "https://data-seed-prebsc-1-s1.binance.org:8545"
+  w3 = Web3(HTTPProvider(bnb_url))
+  assert w3.is_connected(), f"Failed to connect to provider at {bnb_url}"
 
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-    w3.middleware_onion.inject(geth_poa_middleware, layer = 0)
-    contract = w2.eth.contract(address = address, abi = abi)
+  w3.middleware_onion.inject(geth_poa_middleware, layer = 0)
+  contract = w2.eth.contract(address = address, abi = abi)
 
 	return w3, contract
 
