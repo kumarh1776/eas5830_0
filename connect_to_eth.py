2be9af7a19dfa26d873371716bf404b1337a8317
kumarh1776 <59850401+kumarh1776@users.noreply.github.com>
2024-07-02 22:13:42 -0400

    Add files via upload

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -11,35 +11,36 @@ infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
 
 
 def connect_to_eth():
-	url = "https://mainnet.infura.io/v3/1ca518b0b5c2414ca764e1d1dba11465"  # FILL THIS IN
-	w3 = Web3(HTTPProvider(url))
-	assert w3.is_connected(), f"Failed to connect to provider at {url}"
-	return w3
+    url = "https://mainnet.infura.io/v3/1ca518b0b5c2414ca764e1d1dba11465"
+    w3 = Web3(HTTPProvider(url))
+    assert w3.is_connected(), f"Failed to connect to provider at {url}"
+    return w3
 
 
 def connect_with_middleware(contract_json):
-	with open(contract_json, "r") as f:
-		d = json.load(f)
-		d = d['bsc']
-		address = d['address']
-		abi = d['abi']
+    with open(contract_json, "r") as f:
+        d = json.load(f)
+        d = d['bsc']
+        address = d['address']
+        abi = d['abi']
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	bnb_url = "https://data-seed-prebsc-1-s1.binance.org:8545"
-  w3 = Web3(HTTPProvider(bnb_url))
-  assert w3.is_connected()
+    bnb_url = "https://data-seed-prebsc-1-s1.binance.org:8545"
+    w3 = Web3(HTTPProvider(bnb_url))
+    assert w3.is_connected()
 
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-	w3.middleware_onion.inject(geth_poa_middleware, layer = 0)
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-  contract = w3.eth.contract(address = address, abi = abi)
+    contract = w3.eth.contract(address=address, abi=abi)
 
-	return w3, contract
+    return w3, contract
 
 
 if __name__ == "__main__":
-	w3_eth = connect_to_eth()
-  w3_bnb, merkle_validator_contract = connect_with_middleware("contract_info.json")
+    w3_eth = connect_to_eth()
+    w3_bnb, merkle_validator_contract = connect_with_middleware("contract_info.json")
+
